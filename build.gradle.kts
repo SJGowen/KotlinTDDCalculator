e2d3f88f40c3f16d691cff1c8246a4e3e5cd46f0
Stephen <sjgowen@gmail.com>
2026-03-20 14:28:43 +0000

    Upgraded project so kotlin version is 2.3.0, junit-jupiter version to 5.14.3 and strikt-core version to 0.35.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "2.3.0"
 }
 
 repositories {
@@ -8,9 +8,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.14.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("io.strikt:strikt-core:0.34.0")
+    testImplementation("io.strikt:strikt-core:0.35.1")
 }
 
 tasks.test {
